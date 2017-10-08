fd0d37620ac6e2c103e80fef97da04e87e51c4d4
Sandra <Sandra@Sandras-MacBook-Air.local>
2017-10-08 11:57:19 -0400

    Child

--- client/src/pages/Child/Child.js
+++ client/src/pages/Child/Child.js
@@ -4,7 +4,7 @@ import API from "../../utils/API";
 // import {KidDropDown} from "../../components/KidDropDown/KidDropDown.js";
 import {Chore} from "../../components/Chore/Chore.js"
 import {Reward} from "../../components/Reward/Reward.js"
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 
 class Child extends Component {
@@ -14,69 +14,56 @@ class Child extends Component {
     };
     render() {
     return (
-        <div>
-        <div className="navbar">
-        <div className="row">
-        <div className="col-sm-6">
-        <img className="logo" src = "assets/logo.png" alt= "logo" />
-        <span className="chore">ChoreScore</span>  
-        </div>
-        <div className="col-sm-6">
-         
-       </div>
-        </div>
+            <div>
 
-        </div>
-       
-       
-        <div className="row">
-        <div className="col-sm-6">
-           <div className="calendar">
-             <b>
-             <script language="JavaScript">
-
-                    date=Date()
-                    document.write(date)
-
-                    </script>
-            
-             </b>
-           </div> 
-
-           <div className="reward-list">
-           {this.state.rewards.map(reward=>
-            <Reward title={reward.RewardName} points={reward.RewardPoints} />
-            )}
-            <div className="link-btn text-center"> 
-             <Link to="/parent" >
-              Request Reward
-             <img src = "/assets/addChoresBtn.png" alt="add chores button" />
-             </Link> 
-             </div>  
-         </div>
-        </div>
-
-
-
-        <div className="col-sm-6 kid-chores">
-            {this.state.chores.map(chore=>
-          <Chore type="parent" title={chore.taskName} status={chore.RedeemStatus} />
-            )};
-            <div className="link-btn text-right">  
-            <Link to="/parent/addchore" >
-            Add Chores
-            <img src = "/assets/addChoresBtn.png" alt="add chores button" />
-            </Link>        
+            <div className="navbar">
+            <div className="row">
+            <div className="col-sm-6">
+            <img className="logo" src = "assets/logo.png" alt= "logo" />
+            <span className="chore">ChoreScore</span>  
             </div>
-        </div>
-        </div>
+            </div>
+            </div>
+          
+              <div className="col-sm-6">
+             </div>
+            
+
         
+           
+           
+            <div className="row">
+            <div className="col-sm-6">
+             <div className="calendar">
+                 <b>
+                
+                
+                 </b>
+               </div>
+               </div>
+               </div> 
 
 
-        </div>
-);
-    }
-}
+            <div className="reward-list">
+               {this.state.rewards.map(reward=>
+                <Reward title={reward.RewardName} points={reward.RewardPoints} />
+                )}
+                <div className="link-btn text-center"> 
+                 <Link to="/parent" >
+                 Request Rewards
+                 <img src = "/assets/addChoresBtn.png" alt="add chores button" />
+                </Link> 
+                </div>
+                </div>
+
+           </div>
+
+
+
+        )};
+ }
+
+
 
 
 export default Child;
